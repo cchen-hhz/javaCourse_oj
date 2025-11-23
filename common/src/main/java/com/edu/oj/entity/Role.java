9f718c817121042a7110a8e95facc170a789605e
Lukadoncic777777 <931231099@qq.com>
2025-11-23 22:51:19 +0800

    修改 XML 配置

--- common/src/main/java/com/edu/oj/entity/Role.java
+++ common/src/main/java/com/edu/oj/entity/Role.java
@@ -1,14 +1,16 @@
 package com.edu.oj.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public enum Role {
     ROOT("根"),
     ADMIN("管理员"),
     USER("普通用户");
 
     private final String description;
+
+    private Role(String description) {
+        this.description = description;
+    }
 }
