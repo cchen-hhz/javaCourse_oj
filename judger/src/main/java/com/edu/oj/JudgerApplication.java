9f7f2129b18f0e451fc583a9ff39ff06316ad46e
cchen <1120243698@bit.edu.cn>
2025-11-30 11:08:04 +0800

    add judger

---
+++ judger/src/main/java/com/edu/oj/JudgerApplication.java
@@ -0,0 +1,11 @@
+package com.edu.oj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudgerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JudgerApplication.class, args);
+    }
+}
--- security/src/main/java/com/edu/oj/config/RestAuthenticationEntryPoint.java
+++ security/src/main/java/com/edu/oj/config/RestAuthenticationEntryPoint.java
@@ -1,6 +1,5 @@
 package com.edu.oj.config;
 
-import jakarta.annotation.Resource; // 注意这里导入的是 jakarta.annotation.Resource
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
