b11d78ce2e948f98917ea0705f9a6037388c5e70
Lukadoncic777777 <931231099@qq.com>
2025-11-30 13:27:25 +0800

    完成评测记录类

--- common/src/main/java/com/edu/oj/entity/EvaluationRecord.java
+++ common/src/main/java/com/edu/oj/entity/EvaluationRecord.java
@@ -1,4 +1,15 @@
 package com.edu.oj.entity;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class EvaluationRecord {
+    private Long id;
+    private LocalDateTime time;
+    private String status;
+    private Long userId;
+    private String Language;
+    private Short score;
 }
