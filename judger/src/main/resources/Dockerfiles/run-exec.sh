50eb2999e58202a1cc54344159302263cf742562
Lukadoncic777777 <931231099@qq.com>
2025-12-01 22:20:20 +0800

    在代码执行框架中增强执行时间报告功能

--- judger/src/main/resources/Dockerfiles/run-exec.sh
+++ judger/src/main/resources/Dockerfiles/run-exec.sh
@@ -14,4 +14,13 @@ fi
 
 cd /app
 
+# 记录开始和结束时间（纳秒）
+start_ns=$(date +%s%N)
 "${EXE_FILE}" < "${INPUT_FILE}"
+end_ns=$(date +%s%N)
+
+# 计算毫秒
+elapsed_ms=$(( (end_ns - start_ns) / 1000000 ))
+
+# 用一个特殊前缀输出到 stderr，方便 Java 解析，且不影响 stdout
+echo "__OJ_TIME_MS__=${elapsed_ms}" >&2
